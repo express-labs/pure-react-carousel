d59d897707be99ffb4bb9211ac6366006e68e24a
Matthew Toledo <mtoledo@express.com>
2017-06-08 15:19:58 -0400

    test savepoint

--- src/__tests__/general.test.jsx
+++ src/__tests__/general.test.jsx
@@ -1,3 +1,4 @@
+/* eslint {'import/no-named-as-default': 0, 'import/no-named-as-default-member': 0} */
 import React from 'react';
 import { shallow } from 'enzyme';
 import ButtonBack from '../ButtonBack/ButtonBack';
@@ -135,11 +136,20 @@ const components = {
 };
 
 describe('All visual components should pass through props that are not consumed by that component.', () => {
-  Object.keys(components).map((conf) => {
-    it(`The ${conf} component should pass through propery data-foo`, () => {
+  Object.keys(components).forEach((conf) => {
+    it(`The ${conf} component should pass through propery "data-foo"`, () => {
       const { props, component: Component } = components[conf];
       const wrapper = shallow(<Component data-foo="bar" {...props} />);
       expect(wrapper.at(0).prop('data-foo')).toBe('bar');
+    });
+  });
+});
+describe('All visual components should append any supplied className value to the end of className string', () => {
+  Object.keys(components).forEach((conf) => {
+    it(`The ${conf} component should append class "foo" to the end of the className string`, () => {
+      const { props, component: Component } = components[conf];
+      const wrapper = shallow(<Component className="foo" {...props} />);
+      expect(wrapper.at(0).prop('className').split(' ').pop()).toBe('foo');
     });
   });
 });
