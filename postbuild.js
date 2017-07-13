d7fb8c5605a87d5f12c3fe1fb5bfa0753a6458b8
Matthew Toledo <mtoledo@express.com>
2017-07-13 14:55:43 -0400

    fixing warning with css maps

--- postbuild.js
+++ postbuild.js
@@ -2,6 +2,9 @@
 // This script is run automatically when you do `npm run build`. To run this script manually,
 // do `node postbuild.js`
 var replace = require('replace-in-file');
+var escape = require('escape-string-regexp');
+const baseDir = process.cwd();
+
 
 replace({
   files: [
@@ -11,10 +14,10 @@ replace({
     'dist/react-carousel.es.css.map',
   ],
   from: [
-    /sourceMappingURL=index\.cjs\.css\.map/g,
+    /sourceMappingURL=index\.cjs\.css\.map/g, 
     /sourceMappingURL=index\.es\.css\.map/g,
-    /\/Users\/mtoledo\/Personal\/react-carousel/g,
-    /\/Users\/mtoledo\/Personal\/react-carousel/g,
+    new RegExp(escape(baseDir),'g'),
+    new RegExp(escape(baseDir),'g'),
   ],
   to: [
     'sourceMappingURL=react-carousel.cjs.css.map',
