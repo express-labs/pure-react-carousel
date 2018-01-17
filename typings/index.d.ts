b8ddf975d1cdf11548d9d22be83601e2bd0dbcc9
Jedrzej Lewandowski <progreacja@gmail.com>
2018-01-17 11:47:51 +0100

    Added base version of WithStore

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,7 +3,6 @@
 // TypeScript Version: 2.6.2
 
 import * as React from 'react'
-
 import {
   ButtonBack,
   ButtonFirst,
@@ -17,6 +16,14 @@ import {
   Slider
 } from './carouselElements.d'
 
+type ComponentClass<P> = React.ComponentClass<P>;
+type StatelessComponent<P> = React.StatelessComponent<P>;
+type Component<P> = React.ComponentType<P>;
+
+// Diff / Omit taken from https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-311923766
+type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
+type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+
 interface CarouselState {
   currentSlide: number
   disableAnimation: boolean
@@ -25,40 +32,61 @@ interface CarouselState {
   imageSuccessCount: number
   lockOnWindowScroll: boolean
   masterSpinnerThreshold: number
-  naturalSlideHeight: naturalSlideHeight
-  naturalSlideWidth: naturalSlideWidth
-  orientation: orientation
-  slideSize: slideSize
-  slideTraySize: slideTraySize
-  step: step
-  totalSlides: totalSlides
-  touchEnabled: touchEnabled
-  visibleSlides: visibleSlides
+  naturalSlideHeight: number
+  naturalSlideWidth: number
+  orientation: 'horizontal' | 'vertical'
+  slideSize: number
+  slideTraySize: number
+  step: number
+  totalSlides: number
+  touchEnabled: boolean
+  visibleSlides: number
 }
 
+/**
+ * CarouselProvider allows the other carousel components to communicate with each other.
+ * The only required properties are the orientation, naturalSlideWidth, and naturalSlideHeight.
+ * The naturalSlideWidth and naturalSlideHeight are used to create an aspect ratio for each slide.
+ * Since the carousel is responsive by default, it will stretch to fill in the width of it's parent container.
+ * The CarouselProvider must also have children.
+ */
 interface CarouselProviderProps {
   readonly children: React.ReactNode
   readonly className?: string
-  readonly currentSlide?: number
-  readonly disableAnimation?: boolean
-  readonly hasMasterSpinner?: boolean
-  readonly lockOnWindowScroll?: boolean
-  readonly naturalSlideHeight: number
-  readonly naturalSlideWidth: number
-  readonly orientation?: 'horizontal' | 'vertical'
-  readonly step?: number
+  readonly currentSlide?: CarouselState['currentSlide']
+  readonly disableAnimation?: CarouselState['disableAnimation']
+  readonly hasMasterSpinner?: CarouselState['hasMasterSpinner']
+  readonly lockOnWindowScroll?: CarouselState['lockOnWindowScroll']
+  readonly naturalSlideHeight: CarouselState['naturalSlideHeight']
+  readonly naturalSlideWidth: CarouselState['naturalSlideWidth']
+  readonly orientation?: CarouselState['orientation']
+  readonly step?: CarouselState['step']
   readonly tag?: string
-  readonly totalSlides: number
-  readonly touchEnabled?: boolean
-  readonly visibleSlides?: boolean
+  readonly totalSlides: CarouselState['totalSlides']
+  readonly touchEnabled?: CarouselState['touchEnabled']
+  readonly visibleSlides?:  CarouselState['visibleSlides']
 }
+
+
 type CarouselProviderInterface = React.ComponentClass<CarouselProviderProps>
 declare const CarouselProvider: CarouselProviderInterface
 
-interface EnhancedComponent {}
+interface InjectedProps {
+  setStoreState: ({}: CarouselState) => void
+}
+
+export interface InferableComponentEnhancerWithProps<TInjectedProps, TNeedsProps> {
+    <P extends TInjectedProps>(
+        component: Component<P>
+    ): ComponentClass<Omit<P, keyof TInjectedProps> & TNeedsProps> & {WrappedComponent: Component<P>}
+}
+
+
+type InferableComponentEnhancer<TInjectedProps> =
+    InferableComponentEnhancerWithProps<TInjectedProps, {}>
 
 interface WithStoreInterface {
-  (): EnhancedComponent
+  (): InferableComponentEnhancer<InjectedProps>
 }
 
 declare const WithStore: WithStoreInterface
