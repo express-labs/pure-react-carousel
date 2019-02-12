d0b0218f875da05291ba22f5bca790667eeac745
Federico Dionisi <fdionisi@users.noreply.github.com>
2019-02-12 22:35:19 +0100

    Remove `step` attribute from Slider div (#120)

--- src/Slider/Slider.jsx
+++ src/Slider/Slider.jsx
@@ -544,6 +544,9 @@ const Slider = class Slider extends React.Component {
 
     // console.log(Object.assign({}, trayStyle), new Date());
 
+    // remove `step` from Slider div, since it isn't a valid html attribute
+    const { step, ...rest } = props;
+
     return (
       <div
         ref={(el) => { this.sliderElement = el; }}
@@ -553,7 +556,7 @@ const Slider = class Slider extends React.Component {
         tabIndex={newTabIndex}
         onKeyDown={this.handleOnKeyDown}
         role="listbox"
-        {...props}
+        {...rest}
       >
         <div className={trayWrapClasses} style={trayWrapStyle}>
           <TrayTag
