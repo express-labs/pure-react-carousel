ccf28a0a6e5c37bb25661991fc5ea80eabc7df8d
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2019-06-25 14:58:50 +0200

    Feature/dotgroup tuning (#151)
    * Add disableDots props to DotsGroup
    
    In order to always allow the click on dots on DotsGroup if needed
    
    * add dist folder
    
    * fixing DOM problem
    
    * single dot select
    
    * added two props to DotGroup + tests + README

--- src/DotGroup/__tests__/DotGroup.test.jsx
+++ src/DotGroup/__tests__/DotGroup.test.jsx
@@ -34,4 +34,28 @@ describe('<DotGroup />', () => {
     expect(wrapper.find('span').at(1).text()).toEqual('');
     expect(wrapper.find('span').at(2).text()).toEqual('');
   });
+  it('should render enabled active dots if disableActiveDots prop is false', () => {
+    const wrapper = shallow(<DotGroup {...props} disableActiveDots={false} />);
+    expect(wrapper.children().at(0).prop('disabled')).toEqual(false);
+    expect(wrapper.children().at(1).prop('disabled')).toEqual(false);
+    expect(wrapper.children().at(2).prop('disabled')).toEqual(false);
+  });
+  it('should render DISABLED dots if disableActiveDots prop is not set', () => {
+    const wrapper = shallow(<DotGroup {...props} />);
+    expect(wrapper.children().at(0).prop('disabled')).toEqual(false);
+    expect(wrapper.children().at(1).prop('disabled')).toEqual(true);
+    expect(wrapper.children().at(2).prop('disabled')).toEqual(true);
+  });
+  it('should render only current slide dot as selected if showAsSelectedForCurrentSlideOnly prop is true', () => {
+    const wrapper = shallow(<DotGroup {...props} showAsSelectedForCurrentSlideOnly />);
+    expect(wrapper.children().at(0).prop('selected')).toEqual(false);
+    expect(wrapper.children().at(1).prop('selected')).toEqual(true);
+    expect(wrapper.children().at(2).prop('selected')).toEqual(false);
+  });
+  it('should render all visible slides dot as selected if showAsSelectedForCurrentSlideOnly prop is not set', () => {
+    const wrapper = shallow(<DotGroup {...props} />);
+    expect(wrapper.children().at(0).prop('selected')).toEqual(false);
+    expect(wrapper.children().at(1).prop('selected')).toEqual(true);
+    expect(wrapper.children().at(2).prop('selected')).toEqual(true);
+  });
 });
