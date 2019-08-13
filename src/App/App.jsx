1e3fddb7d679805a02a250e6495066c9c53c9933
Matthew Toledo <mtoledo@express.com>
2019-08-13 13:56:47 -0400

    fix incorrect value for preserveAspectRatio

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -68,7 +68,7 @@ class DevelopmentApp extends React.Component {
                       className={s.externalLink}
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
-                      preserveAspectRatio="xMidyMid meet"
+                      preserveAspectRatio="xMidYMid meet"
                     >
                       <path d="M5 3c-1.093 0-2 .907-2 2v14c0 1.093.907 2 2 2h14c1.093 0 2-.907 2-2v-7h-2v7H5V5h7V3H5zm9 0v2h3.586l-9.293 9.293 1.414 1.414L19 6.414V10h2V3h-7z" />
                     </svg>
