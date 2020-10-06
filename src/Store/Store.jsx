73dcbf7885f9ee0466c09c862bafea7a67bec6c0
Niko Savas <nikosavas@hotmail.com>
2020-10-06 16:25:00 -0700

    Update Store.jsx
    Fix lint error

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -89,7 +89,8 @@ const Store = class Store {
   }
 
   isMasterSpinnerFinished() {
-    return Object.keys(this.masterSpinnerSubscriptions).filter(src => this.masterSpinnerSubscriptions[src].complete !== true).length === 0;
+    return Object.keys(this.masterSpinnerSubscriptions).filter(
+      src => this.masterSpinnerSubscriptions[src].complete !== true).length === 0;
   }
 };
 
