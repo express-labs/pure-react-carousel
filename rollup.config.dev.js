91183cadf2b1a02eee308f6b4e178c81df3e6f90
Matthew Toledo <161068+mrbinky3000@users.noreply.github.com>
2022-10-19 22:50:25 -0400

    build(packaging): various updates to build process and security
    * fix the demo page
    
    The demo page was broken because the last deploy was done with the wrong version of node and npm.  Added a .nvmrc file and updated the path for the css file.  Also updated README documentation.
    
    * Adding package-lock.json to repo
    
    Supposedly this was removed to make it easier for contributors to contribute, but after researching the matter the best practice is to always commit a lockfile so that everyone is working from the same source of truth.
    
    * security update for semantic-release
    
    Fixing github security warning
    
    * gitignore the output directory for dev server
    
    * fix css not showing up in local dev server
    
    * these files are now gitignored
    
    * Force people to use the correct node version
    
    * Update ci.yml
    
    set node version to 10.16.1

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -13,6 +13,71 @@ import serve from "rollup-plugin-serve";
 var pkg = require("./package.json");
 var cache;
 
+const plugins = [
+  image({
+    limit: 10000
+  }),
+  postcss({
+    extensions: [".css", ".scss"],
+    extract: "dev/script/style.css",
+    minimize: true,
+    modules: {
+      // customize the name of the css classes that are created
+      generateScopedName: "[local]___[hash:base64:5]"
+    },
+    sourceMap: "inline" // true, "inline" or false
+  }),
+  resolve({
+    browser: true,
+    customResolveOptions: {
+      moduleDirectory: "node_modules"
+    },
+    extensions: [".js", ".jsx"],
+    jsnext: true,
+    main: true,
+    module: true
+  }),
+  replace({ "process.env.NODE_ENV": JSON.stringify("development") }),
+  commonjs({
+    include: ["node_modules/**"],
+    exclude: ["node_modules/process-es6/**"],
+    namedExports: {
+      "node_modules/react/index.js": [
+        "Children",
+        "Component",
+        "Fragment",
+        "PureComponent",
+        "createElement",
+        "lazy",
+        "useState",
+        "Suspense"
+      ],
+      "node_modules/react-dom/index.js": ["render"],
+      "node_modules/react-is/index.js": ["isValidElementType"]
+    }
+  }),
+  eslint({
+    exclude: ["**/*.scss", "**/*.css", "node_modules/**"]
+  }),
+  babel({
+    exclude: ["node_modules/**", "__tests__/**"]
+  }),
+];
+
+if (process.env.npm_lifecycle_event === 'start') {
+  plugins.push(
+    serve({
+      open: true,
+      verbose: true,
+      contentBase: ["dev"],
+      historyApiFallback: false,
+      host: "localhost",
+      port: 10001
+    }),
+    livereload()
+  )
+}
+
 export default {
   input: "src/app.js",
   cache: cache,
@@ -25,63 +90,5 @@ export default {
   },
   // exclude peerDependencies from our bundle, except for react, react-dom, prop-types when dev'ing
   external: Object.keys(omit(pkg.peerDependencies, ["react", "react-dom"])),
-  plugins: [
-    image({
-      limit: 10000
-    }),
-    postcss({
-      extensions: [".css", ".scss"],
-      extract: "dev/style.css",
-      minimize: true,
-      modules: {
-        // customize the name of the css classes that are created
-        generateScopedName: "[local]___[hash:base64:5]"
-      },
-      sourceMap: "inline" // true, "inline" or false
-    }),
-    resolve({
-      browser: true,
-      customResolveOptions: {
-        moduleDirectory: "node_modules"
-      },
-      extensions: [".js", ".jsx"],
-      jsnext: true,
-      main: true,
-      module: true
-    }),
-    replace({ "process.env.NODE_ENV": JSON.stringify("development") }),
-    commonjs({
-      include: ["node_modules/**"],
-      exclude: ["node_modules/process-es6/**"],
-      namedExports: {
-        "node_modules/react/index.js": [
-          "Children",
-          "Component",
-          "Fragment",
-          "PureComponent",
-          "createElement",
-          "lazy",
-          "useState",
-          "Suspense"
-        ],
-        "node_modules/react-dom/index.js": ["render"],
-        "node_modules/react-is/index.js": ["isValidElementType"]
-      }
-    }),
-    eslint({
-      exclude: ["**/*.scss", "**/*.css", "node_modules/**"]
-    }),
-    babel({
-      exclude: ["node_modules/**", "__tests__/**"]
-    }),
-    serve({
-      open: true,
-      verbose: true,
-      contentBase: ["dev"],
-      historyApiFallback: false,
-      host: "localhost",
-      port: 10001
-    }),
-    livereload()
-  ]
+  plugins
 };
